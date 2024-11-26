cf1b96829af36aa6115459bc9468b2c3e69fc8f0
Aritra Chakraborty <chakraritra@gmail.com>
2024-11-26 23:35:48 +0530

    revised card component in about

--- PksFilmer/src/components/About_Compo/CardFlip.jsx
+++ PksFilmer/src/components/About_Compo/CardFlip.jsx
@@ -6,13 +6,13 @@ const CardFlip = () => {
   const [hoveredCard, setHoveredCard] = useState(null);
 
   return (
-    <div className="text-center grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-16 ml-12 gap-y-12 p-4">
+    <div className="text-center grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-12 gap-x-16 mt-20 mx-14 ">
       {aboutCard.map((card) => (
         <div
           key={card.id}
           onMouseEnter={() => setHoveredCard(card.id)}
           onMouseLeave={() => setHoveredCard(null)}
-          className="relative perspective w-[50vh] h-[50vh]"
+          className="relative perspective w-[70vh] h-[50vh]"
         >
           <div
             className={`relative w-full h-full transform-style-preserve-3d transition-transform duration-500 ${
@@ -21,21 +21,22 @@ const CardFlip = () => {
           >
             {/* Front Side */}
             <div className="absolute backface-hidden w-full h-full flex flex-col items-center justify-center border-8 border-white">
-              <h2 className="font-abril font-bold text-2xl">
+              <h2 className="font-abril font-bold text-4xl">
                 {card.frontHeading}
               </h2>
-              <p className="text-xl">{card.frontContent}</p>
+              <p className="text-xl mt-3">{card.frontContent}</p>
             </div>
             {/* Back Side */}
             <div className="absolute backface-hidden w-full h-full flex flex-col items-center justify-center border-8 border-white  rotate-y-180">
-              <h2 className="font-abril font-bold text-2xl">
+              <h2 className="font-abril font-bold text-4xl">
                 {card.backHeading}
               </h2>
-              <p className="text-xl">{card.backContent}</p>
+              <p className="text-xl mt-3">{card.backContent}</p>
             </div>
           </div>
         </div>
       ))}
+      
     </div>
   );
 };
