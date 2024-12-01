fe5ec3ecd72538dff577047c2be68b6803f39eb5
Aritra Chakraborty <chakraritra@gmail.com>
2024-12-01 11:39:58 +0530

    corrected carousel

--- PksFilmer/src/components/Carousel_compo/Carousel.jsx
+++ PksFilmer/src/components/Carousel_compo/Carousel.jsx
@@ -16,13 +16,19 @@ const Carousel = ({ slides }) => {
   };
 
   return (
-    <div id="default-carousel" className="relative w-full h-[100vh]" data-carousel="slide">
+    <div
+      id="default-carousel"
+      className="relative w-full h-[100vh]"
+      data-carousel="slide"
+    >
       {/* Carousel slides */}
       <div className="relative w-full h-full overflow-hidden">
         {slides.map((slide, index) => (
           <div
             key={index}
-            className={`duration-1000 ease-in-out ${index === currentSlide ? "" : "hidden"}`}
+            className={`duration-1000 ease-in-out ${
+              index === currentSlide ? "" : "hidden"
+            }`}
             data-carousel-item
           >
             <img
@@ -35,12 +41,14 @@ const Carousel = ({ slides }) => {
       </div>
 
       {/* Indicators */}
-      <div className="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
+      <div className="absolute top-auto z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
         {slides.map((_, index) => (
           <button
             key={index}
             type="button"
-            className={`w-3 h-3 rounded-full ${index === currentSlide ? "bg-white" : "bg-gray-800"}`}
+            className={`w-3 h-3 rounded-full ${
+              index === currentSlide ? "bg-white" : "bg-gray-800"
+            }`}
             aria-current={index === currentSlide ? "true" : "false"}
             aria-label={`Slide ${index + 1}`}
             data-carousel-slide-to={index}
@@ -49,12 +57,14 @@ const Carousel = ({ slides }) => {
         ))}
       </div>
 
-      {/* Previous Button */}
+      {/* Previous and Next Buttons */}
       <button
         type="button"
-        className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+        className="absolute top-1/2 left-0 z-30 flex items-center justify-center h-10 px-4 -translate-y-1/2 cursor-pointer group focus:outline-none"
         data-carousel-prev
-        onClick={() => handleSlideChange((currentSlide - 1 + slides.length) % slides.length)}
+        onClick={() =>
+          handleSlideChange((currentSlide - 1 + slides.length) % slides.length)
+        }
       >
         <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
           <svg
@@ -76,16 +86,17 @@ const Carousel = ({ slides }) => {
         </span>
       </button>
 
-      {/* Next Button */}
       <button
         type="button"
-        className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+        className="absolute top-1/2 right-0 sm:left-[90%] lg:left-[93%] z-30 flex items-center justify-center h-10 px-4 -translate-y-1/2 cursor-pointer group focus:outline-none"
         data-carousel-next
-        onClick={() => handleSlideChange((currentSlide + 1) % slides.length)}
+        onClick={() =>
+          handleSlideChange((currentSlide + 1) % slides.length)
+        }
       >
         <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
           <svg
-            className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+            className="w-4 h-4 text-white dark:text-gray-800  "
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
