654c932910f8ce2bc10482311584fa7e4a5b2981
Aritra Chakraborty <chakraritra@gmail.com>
2025-01-03 15:47:14 +0530

    made the gallery  page fully responsive for all devices

--- PksFilmer/src/pages/Gallery_Page/gallery.jsx
+++ PksFilmer/src/pages/Gallery_Page/gallery.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import {Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import galleyMainPhoto from "../../assets/Pks_Photos/PKs_Clean_Photos/Gallery/galleryPhoto.jpg";
 import MasonaryGrid from "../../components/Gallery_Compo/MasonaryGrid";
-import QuickLinks from "../../components/Hero_Section/QuickLinks"
+import QuickLinks from "../../components/Hero_Section/QuickLinks";
 
 const gallery = () => {
   return (
@@ -20,24 +20,19 @@ const gallery = () => {
             filter: "blur(1px)",
           }}
         ></div>
-
         {/* Text Container */}
-        <div
-          className="relative z-20 p-18 lg:px-20 text-white"
-          style={{ marginTop: "19vh" }} // Adjust positioning if needed
-        >
-          <div className="text-down_left ml-5">
+        <div className="relative z-20 px-8 py-18 lg:px-20 text-white md:mt-[19vh] mt-[12vh]">
+          <div className="text-left mx-auto">
             <h2
-              className="text-4xl font-bold mt-16 "
+              className="text-4xl font-bold mt-16"
               style={{ textShadow: "2px 2px 4px rgba(1, 1, 1, 1)" }}
             >
               PORTFOLIO
             </h2>
             <p
-              className="text-lg"
+              className="text-lg mt-6"
               style={{ textShadow: "1px 1px 3px rgba(0, 0, 0, 0.6)" }}
             >
-              <span className="text-yellow-400 text-8xl">“</span>
               At PKS Filmer, we believe photography is about telling stories and
               preserving emotions that last a lifetime. From joyful celebrations
               to heartfelt moments, we capture each detail with creativity and
@@ -46,38 +41,40 @@ const gallery = () => {
           </div>
         </div>
       </div>
-      <div className="text-left m-24">
-        <h2 className="text-4xl font-bold mb-4 flex items-center justify-center">
+      <div className="mt-24 mx-4 md:mx-14 text-center md:text-center">
+        <h2 className="text-3xl font-bold mb-4 md:text-5xl">
           Our Latest Captures
         </h2>
-        <p className="text-lg leading-relaxed">
-          <span className="text-yellow-400 text-8xl">"</span>We’re here to
-          capture every cherished memory. From weddings to pre-weddings, we
-          preserve your story, from your vows to candid moments of joy. Kid
-          photography captures pure innocence, while event photography ensures
-          every occasion shines. For product portfolios, we highlight the best
-          details, making your brand stand out. Each frame is crafted with
-          passion and professionalism, so your moments stay timeless, reflecting
-          the beauty and joy of every occasion.
+        <p className="text-lg leading-relaxed mt-10 ">
+          We’re here to capture every cherished memory. From weddings to
+          pre-weddings, we preserve your story, from your vows to candid moments
+          of joy. Kid photography captures pure innocence, while event
+          photography ensures every occasion shines. For product portfolios, we
+          highlight the best details, making your brand stand out. Each frame is
+          crafted with passion and professionalism, so your moments stay
+          timeless, reflecting the beauty and joy of every occasion.
         </p>
       </div>
-      <div className="-mt-12">
+      <div className="mt-12">
         <MasonaryGrid />
       </div>
-      <div className="bg-[#4ca9b4] h-[60vh]  font-abril mb-5 flex flex-col items-center justify-center">
-        <h2 className="text-5xl">
+      <div className="bg-[#4ca9b4] md:h-[80vh] h-[50vh] md:mt-24 mt-14 font-abril flex flex-col items-center justify-center md:items-center md:justify-center text-center">
+        <h2 className="text-3xl md:text-5xl">
+          {" "}
           Bringing your vision to life through every shot.
         </h2>
-        <p className="font-roboto mt-8 text-2xl">
-          Through creative ideas, innovation & sheer determination
+        <p className="font-roboto mt-8 text-xl mx-11 text-gray-300 tracking-widest">
+          THROUGH CREATIVE IDEAS, INNOVATION & SHEER DETERMINATION
         </p>
         <Link to="/contact">
-          <button className="mt-8 font-display text-lg font-semibold bg-transparent text-white py-2 px-6 border-2 border-white hover:bg-yellow-500">
+          <button className="mt-8 font-display text-lg font-semibold bg-transparent text-white py-2 px-6 border-2 border-white hover:bg-yellow-500 ">
             CONTACT US
           </button>
         </Link>
       </div>
-      <QuickLinks />
+      <div>
+        <QuickLinks />
+      </div>
     </div>
   );
 };
